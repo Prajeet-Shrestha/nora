d529241cf16d6dca2c769be8a435fb90ee937267
Prajeet-Shrestha <prazzeettstha@gmail.com>
2021-08-06 11:43:33 +0545

    url

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -5,7 +5,7 @@ import { HttpService } from '../http/http.service';
 })
 export class ChatService {
   constructor(private _http: HttpService) {}
-  url = '';
+  url = 'http://127.0.0.1:5000/chat';
   getChatResponse(message) {
     return this._http.post(
       this.url,
